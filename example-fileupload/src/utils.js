8503a92ff2e5df12f288c4e955ef6ddc3ce00421
Bradley Schofield <ionicisere@gmail.com>
2020-10-03 13:29:56 +0100

    Update utils.js

--- example-fileupload/src/utils.js
+++ example-fileupload/src/utils.js
@@ -5,4 +5,12 @@ appwrite
   .setEndpoint('ENDPOINTURL') // Replace with your own endpoint
   .setProject('PROJECTURL') // Replace with your own ProjectID
 
+  let promise = sdk.account.createSession('emailaddress', 'password');
+
+  promise.then(function (response) {
+      console.log(`Successfully logged in as: ${response.name}`); // Success
+  }, function (error) {
+      console.error(error); // Failure
+  });
+
 export { appwrite }
