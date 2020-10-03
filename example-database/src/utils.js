139d6926c5c7018d7b50ff42d8f2a78123cd4088
Bradley Schofield <ionicisere@gmail.com>
2020-10-03 13:27:51 +0100

    Update utils.js

--- example-database/src/utils.js
+++ example-database/src/utils.js
@@ -5,4 +5,12 @@ appwrite
   .setEndpoint('ENDPOINT URL') // Replace this with your endpoint
   .setProject('PROJECTID') // Replace this with your projectID
 
+  let promise = sdk.account.createSession('emailaddress', 'password');
+
+  promise.then(function (response) {
+      console.log(`Successfully logged in as: ${response.name}`); // Success
+  }, function (error) {
+      console.error(error); // Failure
+  });
+
 export { appwrite }
