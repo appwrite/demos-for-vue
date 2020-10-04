f1eecbb7d9c0e236e1686deaaa85ef2b260c6b7a
k-guy-dev <56889834+k-guy-dev@users.noreply.github.com>
2020-10-05 01:20:00 +0530

    Update utils.js

--- example-locale_api/src/utils.js
+++ example-locale_api/src/utils.js
@@ -3,7 +3,7 @@ import * as Appwrite from "appwrite";
 let sdk = new Appwrite();
 
 sdk
-  .setEndpoint("http://localhost:443/v1") // Your API Endpoint
-  .setProject("5f7725e84c447"); // Your project ID
+  .setEndpoint("[YOUR API ENDPOINT]") // Your API Endpoint
+  .setProject("[YOUR PROJECT ID]"); // Your project ID
 
 export {sdk};
